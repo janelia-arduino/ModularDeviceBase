ebb935a974f42619c9a2246c1f5d3124d267d74b
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:47 -0400

    Removed unnecessary header includes.

--- src/ModularDeviceBase.h
+++ src/ModularDeviceBase.h
@@ -7,20 +7,12 @@
 // ----------------------------------------------------------------------------
 #ifndef MODULAR_DEVICE_BASE_H
 #define MODULAR_DEVICE_BASE_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
 #include <Watchdog.h>
 
 #include <ModularClient.h>
