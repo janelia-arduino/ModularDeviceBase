819441a37903a8b76a93a6a4c5d2661d9b89216c
Peter Polidoro <peterpolidoro@gmail.com>
2018-08-12 12:54:07 -0400

    polidorop@janelia.hhmi.org -> peterpolidoro@gmail.com

--- src/ModularDeviceBase/ClientStream.h
+++ src/ModularDeviceBase/ClientStream.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro polidorop@janelia.hhmi.org
+// Peter Polidoro peterpolidoro@gmail.com
 // ----------------------------------------------------------------------------
 #ifndef _CLIENT_STREAM_H_
 #define _CLIENT_STREAM_H_
